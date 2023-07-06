ad1172600e4ae7156ce979272ea1576f4acd2c4d
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-06 09:39:09 +0800

    created ClicksSchema

---
+++ src/models/Clicks.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const clicksDb = mongoose.connection.useDb("Clicks");
+
+const ClickSchema = new Schema(
+    {
+        url: {
+            type: Schema.Types.ObjectId,
+            ref: "Url",
+            required: true,
+        },
+        location: {
+            type: String,
+            required: false,
+        },
+        ip: {
+            type: String,
+            required: false,
+        },
+        os: {
+            type: String,
+            required: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const Clicks = clicksDb.model("Clicks", ClickSchema);
+export default Clicks;

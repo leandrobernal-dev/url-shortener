fab9b01bfdc3db2165256e1b773f9b789dfd4aef
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-06 09:40:25 +0800

    minor bug fixes + using virtual schemas and populate() method to query related data

---
+++ src/models/Url.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import Clicks from "./Clicks";
+
+const urlsDb = mongoose.connection.useDb("Urls");
+
+export const UrlSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        name: {
+            type: String,
+            required: false,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+        shortenedUrl: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+UrlSchema.virtual("clicks", {
+    ref: Clicks,
+    localField: "_id",
+    foreignField: "url",
+    count: true,
+});
+
+const Url = urlsDb.model("Urls", UrlSchema);
+export default Url;

99afeaba943f2364dce4f3ea19f5cae1e1c2590d
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-06 09:34:22 +0800

    added url redirect functionality + saving clicks data

--- src/app/[redirect]/page.js
+++ src/app/[redirect]/page.js
@@ -1,11 +1,17 @@
 import { notFound, redirect } from "next/navigation";
-import Url from "@/models/urlsModel";
+import Url from "@/models/Url";
+import Clicks from "@/models/Clicks";
 
 export default async function RedirectPage(request) {
     const redirectURLId = request.params.redirect;
     const url = await Url.findOne({ shortenedUrl: redirectURLId });
 
     if (url) {
+        const newClick = new Clicks({
+            url: url._id,
+            location: null,
+        });
+        await newClick.save();
         redirect(url.url);
     }
 

fab7235e1998d405ec1119b6f296ff208d127edf
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-19 09:05:04 +0800

    custom chart js label design

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -1,9 +1,7 @@
 import Chart from "chart.js/auto";
 import React, { useEffect, useRef } from "react";
-
 const DoughnutChart = ({ data }) => {
     const chartRef = useRef(null);
-
     useEffect(() => {
         const ctx = chartRef.current.getContext("2d");
         const pieChart = new Chart(ctx, {
@@ -24,6 +22,40 @@ const DoughnutChart = ({ data }) => {
                     legend: {
                         position: "right",
                         align: "start",
+                        labels: {
+                            generateLabels: (chart) => {
+                                let data = chart.data;
+                                if (
+                                    data.labels.length &&
+                                    data.datasets.length
+                                ) {
+                                    return data.labels.map(function (
+                                        label,
+                                        index
+                                    ) {
+                                        let dataset = data.datasets[0];
+                                        let currentValue = dataset.data[index];
+                                        return {
+                                            text: label + ": " + currentValue,
+                                            fillStyle:
+                                                dataset.backgroundColor[index],
+                                            hidden: chart.getDatasetMeta(0)
+                                                .data[index].hidden,
+                                            lineCap: "round",
+                                            lineDash: [],
+                                            lineDashOffset: 0,
+                                            lineJoin: "round",
+                                            lineWidth: 0,
+                                            strokeStyle: dataset.borderColor,
+                                            pointStyle: dataset.pointStyle,
+                                            rotation: 0,
+                                            fontColor: "gray",
+                                        };
+                                    });
+                                }
+                                return [];
+                            },
+                        },
                     },
                     title: {
                         display: true,

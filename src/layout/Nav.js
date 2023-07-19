4d7a306d12aee04eac630cd56d9ff0e534df0cf8
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-19 14:09:44 +0800

    sidebar toggle icon fixed positioning

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -3,7 +3,7 @@ import { Menu } from "@mui/icons-material";
 
 export default function Nav({ setIsOpen }) {
 	return (
-		<nav className="flex h-14 border-b px-2 shadow dark:border-border dark:bg-secondary dark:text-white">
+		<nav className="flex h-14 items-center border-b px-2 shadow dark:border-border dark:bg-secondary dark:text-white">
 			<div className="flex h-14 items-center md:hidden">
 				<button
 					className="p-2"

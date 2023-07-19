62457e93daa14d618505dba699d9a264e926b10f
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-19 22:15:11 +0800

    added error detection

--- src/app/api/geturltitle/route.js
+++ src/app/api/geturltitle/route.js
@@ -4,14 +4,19 @@ import axios from "axios";
 import { load } from "cheerio";
 
 export const GET = async (req) => {
-    const { searchParams } = new URL(req.url);
-    const url = searchParams.get("url");
-    const { title, status } = await axios.get(url).then((response) => {
-        const html = response.data;
-        const $ = load(html);
-        const title = $("title").text();
-        const status = response.status;
-        return { title, status };
-    });
-    return NextResponse.json({ title, status });
+	const { searchParams } = new URL(req.url);
+	const url = searchParams.get("url");
+
+	try {
+		const { title, status } = await axios.get(url).then((response) => {
+			const html = response.data;
+			const $ = load(html);
+			const status = response.status;
+			const title = $("title").text();
+			return { title, status };
+		});
+		return NextResponse.json({ title, status });
+	} catch (error) {
+		return NextResponse.json({ title: "" });
+	}
 };

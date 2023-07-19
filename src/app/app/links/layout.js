3bb55a7eaefad91ed55ae888d52c1dcdcff8b880
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-19 15:02:31 +0800

    added sideBarOpen context to access to [url] page

--- src/app/app/links/layout.js
+++ src/app/app/links/layout.js
@@ -1,8 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useRouter, useSelectedLayoutSegment } from "next/navigation";
 import Loading from "@/components/Loading";
+
+export const SelectedLinkContext = createContext();
 
 export default function App({ children }) {
 	const router = useRouter();
@@ -32,54 +34,64 @@ export default function App({ children }) {
 	}, []);
 
 	return (
-		<main className="flex h-full w-full dark:text-white">
-			<div className="flex w-64 flex-col gap-3 border-r p-1 shadow dark:border-border dark:bg-tertiary">
-				{isLoading ? (
-					<Loading />
-				) : (
-					<>
-						{data.map((url) => {
-							return (
-								<button
-									key={url._id}
-									onClick={() => {
-										setActiveLink(url.shortenedUrl);
-										router.push(
-											`/app/links/${url.shortenedUrl}`,
-										);
-									}}
-									className={` relative flex rounded p-2 ${
-										activeLink === url.shortenedUrl
-											? "shadow-lg dark:bg-primary"
-											: ""
-									}`}
-								>
-									<span className="absolute left-0 top-0 z-0 flex h-full w-8 items-center justify-center dark:bg-slate-600">
-										<input type="checkbox" name="" id="" />
-									</span>
-
-									<div className="flex flex-1 flex-col justify-start">
-										<span>
-											{new Date(
-												url.createdAt,
-											).toLocaleDateString("en-US", {
-												month: "short",
-												day: "numeric",
-											})}
+		<SelectedLinkContext.Provider value={{ activeLink, setActiveLink }}>
+			<main className="flex h-full w-full dark:text-white">
+				<div className="flex w-full flex-col gap-3 border-r p-1 shadow dark:border-border dark:bg-tertiary sm:w-64">
+					{isLoading ? (
+						<Loading />
+					) : (
+						<>
+							{data.map((url) => {
+								return (
+									<button
+										key={url._id}
+										onClick={() => {
+											setActiveLink(url.shortenedUrl);
+											router.push(
+												`/app/links/${url.shortenedUrl}`,
+											);
+										}}
+										className={` relative flex rounded p-2 ${
+											activeLink === url.shortenedUrl
+												? "shadow-lg dark:bg-primary"
+												: ""
+										}`}
+									>
+										<span className="absolute left-0 top-0 z-0 flex h-full w-8 items-center justify-center dark:bg-slate-600">
+											<input
+												type="checkbox"
+												name=""
+												id=""
+											/>
 										</span>
-										<span>{url.name}</span>
-									</div>
-								</button>
-							);
-						})}
-					</>
-				)}
-			</div>
 
-			<div className="w-full flex-1">
-				{children}
-				{/* <Suspense fallback={<Loading />}></Suspense> */}
-			</div>
-		</main>
+										<div className="flex flex-1 flex-col justify-start">
+											<span>
+												{new Date(
+													url.createdAt,
+												).toLocaleDateString("en-US", {
+													month: "short",
+													day: "numeric",
+												})}
+											</span>
+											<span>{url.name}</span>
+										</div>
+									</button>
+								);
+							})}
+						</>
+					)}
+				</div>
+
+				<div
+					className={`fixed left-0 top-0 h-full w-full bg-white dark:bg-primary sm:relative sm:flex-1 ${
+						segment ? "" : "hidden"
+					}`}
+				>
+					{children}
+					{/* <Suspense fallback={<Loading />}></Suspense> */}
+				</div>
+			</main>
+		</SelectedLinkContext.Provider>
 	);
 }

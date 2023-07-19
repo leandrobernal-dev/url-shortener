cf3706e5650a1187ccec596b33cd68d2aae743c2
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-19 15:11:36 +0800

    minor update, hide sideBar when navigating and clicking create new url button on mobile devices

--- src/app/app/layout.js
+++ src/app/app/layout.js
@@ -10,24 +10,26 @@ export default function AppLayout({ children }) {
 	const [sideBarIsOpen, setSideBarIsOpen] = useState(true);
 
 	useEffect(() => {
-		setSideBarIsOpen(() => (window.innerWidth < 1200 ? false : true));
-		window.addEventListener("resize", handleWindowResize);
+		setSideBarIsOpen(() => (window.innerWidth < 1280 ? false : true));
+		window.addEventListener("resize", handleSideBarToggle);
 		return () => {
-			window.removeEventListener("resize", handleWindowResize);
+			window.removeEventListener("resize", handleSideBarToggle);
 		};
 	}, []);
-	function handleWindowResize() {
+	function handleSideBarToggle() {
 		setSideBarIsOpen(() => (window.innerWidth < 1280 ? false : true));
 	}
 
 	const [newUrlModalOpen, setNewUrlModalOpen] = useState(false);
 	function toggleNewUrlModal() {
+		handleSideBarToggle();
 		setNewUrlModalOpen((prevState) => !prevState);
 	}
 
 	return (
 		<div className={`flex h-screen`}>
 			<SideBar
+				handleSideBarToggle={handleSideBarToggle}
 				toggleNewUrlModalForm={toggleNewUrlModal}
 				isOpen={sideBarIsOpen}
 				setIsOpen={setSideBarIsOpen}

e84a051ce3ac96ca833a3a789e984445b2a57e5e
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-20 11:15:48 +0800

    fixed main padding based on sidebar width

--- src/app/app/layout.js
+++ src/app/app/layout.js
@@ -29,22 +29,38 @@ export default function AppLayout({ children }) {
 		handleSideBarToggle();
 		setNewUrlModalOpen((prevState) => !prevState);
 	}
+	const [sideBarWidth, setSideBarWidth] = useState("64");
+	useEffect(() => {
+		setSideBarWidth(sideBarIsOpen ? "64" : "14");
+	}, [sideBarIsOpen]);
+
+	const navHeight = "14";
 
 	return (
 		<UserDataContextProvider
 			value={{ activeLink, setActiveLink, data, setData }}
 		>
-			<div className={`flex h-screen`}>
+			<div
+				className={`pl-0 pt-14
+				${sideBarIsOpen ? "md:pl-64" : "md:pl-14"}
+			`}
+			>
 				<SideBar
+					width={sideBarWidth}
 					handleSideBarToggle={handleSideBarToggle}
 					toggleNewUrlModalForm={toggleNewUrlModal}
 					isOpen={sideBarIsOpen}
 					setIsOpen={setSideBarIsOpen}
 				/>
 
-				<div className="z-0 flex h-screen flex-1 flex-col overflow-hidden">
-					<Nav setIsOpen={setSideBarIsOpen} />
-					<div className="h-full flex-1 border-b dark:text-white">
+				<div className={`z-0`}>
+					<Nav
+						sideBarOpen={sideBarIsOpen}
+						sideBarWidth={sideBarWidth}
+						height={navHeight}
+						setIsOpen={setSideBarIsOpen}
+					/>
+					<div className={`border-b dark:text-white`}>
 						<Suspense fallback={<Loading />}>{children}</Suspense>
 					</div>
 				</div>

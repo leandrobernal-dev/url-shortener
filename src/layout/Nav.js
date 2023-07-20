fe44d6f769a52d622dea1e86fbeb9bd04090cd07
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-07-20 11:01:09 +0800

    responsive fixed nav

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,9 +1,13 @@
 import ThemeToggle from "@/theme/ThemeToggle";
 import { Menu } from "@mui/icons-material";
 
-export default function Nav({ setIsOpen }) {
+export default function Nav({ setIsOpen, height, sideBarOpen }) {
 	return (
-		<nav className="flex h-14 items-center border-b px-2 shadow dark:border-border dark:bg-secondary dark:text-white">
+		<nav
+			className={`fixed left-0 ${
+				sideBarOpen ? "md:left-64" : "md:left-14"
+			} right-0 top-0 z-10 flex w-full h-${height} items-center border-b px-2 shadow dark:border-border dark:bg-secondary dark:text-white`}
+		>
 			<div className="flex h-14 items-center md:hidden">
 				<button
 					className="p-2"
